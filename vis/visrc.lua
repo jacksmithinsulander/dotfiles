5b9fe3dbe36aba66bfca6a00dae422d8f81ba649
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-03-06 22:04:11 +0100

    Fixed my visrc

--- vis/visrc.lua
+++ vis/visrc.lua
@@ -1,9 +1,17 @@
 require('vis')
 
+leader = '<Space>'
+
 vis.events.subscribe(vis.events.INIT, function()
-	-- Your global configuration options
 	vis:command('set escdelay 0')
 	vis:command('set tabwidth 4')
 	vis:command('set autoindent on')
 	vis:command('set theme dark-16')
 end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	vis:command('set number')
+	vis:command('set relativenumbers')
+	vis:command('set colorcolumn 80')
+end)
+
